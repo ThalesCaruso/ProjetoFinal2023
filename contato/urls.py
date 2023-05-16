6bff8b231d390c194848c43be2a0e6c5105efed7
Thales <carusothales@gmail.com>
2023-05-15 21:32:12 -0300

    Finalização da parte de contato, config SMTP e privacidade dos dados

--- contato/urls.py
+++ contato/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.contato, name="contato"),
+    path('mensagem', views.processa_contato, name="mensagem"),
 ]
