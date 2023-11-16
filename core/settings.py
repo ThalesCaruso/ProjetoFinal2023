b01c6d59a3f5715219620472bb8132e53484e8af
Thales Caruso <79548139+ThalesCaruso@users.noreply.github.com>
2023-11-16 13:37:45 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -100,22 +100,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-   "default": {
-       "ENGINE": "django.db.backends.postgresql",
-       "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "FHDE4RQBFIPxxvRJq1uO",
-        "HOST": "containers-us-west-30.railway.app",
-       "PORT": "7641",
-        'OPTIONS': {
-           # 'options': '-c search_path=railway'
-                },
-    }
-}
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -181,8 +165,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # google config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'carusothales@gmail.com'
-EMAIL_HOST_PASSWORD = 'eubijjjhliuqwmfw'
+EMAIL_HOST_USER = 'example@email.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
