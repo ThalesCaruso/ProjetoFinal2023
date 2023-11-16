5b0a8075d0f16e293de47cf509d84dfc7d97d00a
Thales Caruso <79548139+ThalesCaruso@users.noreply.github.com>
2023-11-16 13:46:23 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -164,10 +164,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # google config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.example.com'
 EMAIL_HOST_USER = 'example@email.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = '#'
+EMAIL_PORT = ###
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
 
